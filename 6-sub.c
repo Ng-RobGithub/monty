f360e1d7423c69734105bed68d0dd70c69f50a04
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-24 14:47:28 -0700

    sub subtracts the top element of the stack

---
+++ 6-sub.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * sub - Subtracts the top element of the stack from the second top element.
+ * @stack: Double pointer to the beginning of the stack.
+ * @line_number: Line number being executed from script file.
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n -= (*stack)->n;
+	pop(stack, line_number);
+}
