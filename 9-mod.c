2f16b02a478981887b5919fb6445a9ce690a3660
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-24 16:31:36 -0700

    mod computes the rest of the division of the second top element of the stack

---
+++ 9-mod.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * _mod - Computes the rest of the division of the second top element
+ * of the stack by the top element.
+ * @stack: Double pointer to the beginning of the stack.
+ * @line_number: Line number being executed from script file.
+ */
+void _mod(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n %= (*stack)->n;
+	pop(stack, line_number);
+}
