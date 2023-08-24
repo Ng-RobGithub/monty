037b3ba65a56ce8e4e93e87c2d4df0633616a49e
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-24 13:56:41 -0700

    add adds the top two elements of the stack.

---
+++ 2-add.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * add - Adds the top two elements of the stack.
+ * @stack: Double pointer to the beginning of the stack.
+ * @line_number: Line number being executed from script file.
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	sum = (*stack)->n + (*stack)->next->n;
+	pop(stack, line_number); /* Remove top element */
+	(*stack)->n = sum;
+}
