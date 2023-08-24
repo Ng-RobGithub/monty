740e36e2865d5f830f92fcc053993287ae78e804
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-24 15:53:19 -0700

    add adds the top two elements of the stack.

--- 8-mul.c
+++ 8-mul.c
@@ -10,7 +10,7 @@ void mul(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
-		free_stack(*stack);
+
 		exit(EXIT_FAILURE);
 	}
 
